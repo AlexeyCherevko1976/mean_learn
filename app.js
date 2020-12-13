40c204885445444e56c6db0031efc14ab9505826
Alexey Cherevko <you@example.com>
2020-12-13 09:37:31 +0700

    854 20:30 podkluchenie k Mongodb

---
+++ app.js
@@ -0,0 +1,24 @@
+const express=require ('express');
+const mongoose=require('mongoose');
+const config=require('config');
+// mongodb+srv://cherevkoalexey1976:<password>@cluster0.zxuz2.mongodb.net/<dbname>?retryWrites=true&w=majority
+const app=express();
+const PORT=config.get('port') || 5000;
+
+async function start(){
+	try{//console.log(config.get('mongoUri'), PORT)
+		await mongoose.connect(config.get('mongoUri'), 
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useCreateIndex: true
+			})
+		app.listen(PORT, ()=>console.log(`App has been started on port ${PORT}`))
+		
+	}catch(e){
+		console.log('Server Error', e.message);
+		//process.exit(code:1);
+	}
+}
+
+start();
