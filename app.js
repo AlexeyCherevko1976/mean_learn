beb5e1017a3d7027db231a519b9b8243d4e108bf
Alexey Cherevko <you@example.com>
2020-12-14 16:31:59 +0700

    53:01 react i node zarabotali

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose=require('mongoose');
 const config=require('config');
 // mongodb+srv://cherevkoalexey1976:<password>@cluster0.zxuz2.mongodb.net/<dbname>?retryWrites=true&w=majority
 const app=express();
+
+app.use('/api/auth', require('./routes/auth.routes'))
 const PORT=config.get('port') || 5000;
 
 async function start(){
